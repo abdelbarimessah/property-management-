f990d123a43a39a714da8932e23aeb2065236bea
abdelbarimessah <93528263+abdelbarimessah@users.noreply.github.com>
2024-09-21 18:04:19 +0100

    feat: the database connection with prisma, Docker complete the connection of the db with primsa and docker compose and start the logger
    try: to add the connection between the frontend
    and the backend

---
+++ backend/prisma/migrations/20240921164545_/migration.sql
@@ -0,0 +1,66 @@
+-- CreateEnum
+CREATE TYPE "PropertyType" AS ENUM ('apartment', 'house');
+
+-- CreateTable
+CREATE TABLE "Property" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "address" VARCHAR(255) NOT NULL,
+    "type" "PropertyType" NOT NULL,
+    "number_of_units" INTEGER NOT NULL DEFAULT 0,
+    "rental_cost" DECIMAL(10,2) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Property_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tenant" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "contact_details" VARCHAR(255) NOT NULL,
+    "property_id" INTEGER NOT NULL,
+    "section" VARCHAR(50),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Tenant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RentalPayment" (
+    "id" SERIAL NOT NULL,
+    "tenant_id" INTEGER NOT NULL,
+    "amount" DECIMAL(10,2) NOT NULL,
+    "payment_date" TIMESTAMP(3) NOT NULL,
+    "settled" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "RentalPayment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PropertyManager" (
+    "id" SERIAL NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+    "password_hash" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PropertyManager_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PropertyManager_username_key" ON "PropertyManager"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PropertyManager_email_key" ON "PropertyManager"("email");
+
+-- AddForeignKey
+ALTER TABLE "Tenant" ADD CONSTRAINT "Tenant_property_id_fkey" FOREIGN KEY ("property_id") REFERENCES "Property"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RentalPayment" ADD CONSTRAINT "RentalPayment_tenant_id_fkey" FOREIGN KEY ("tenant_id") REFERENCES "Tenant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
